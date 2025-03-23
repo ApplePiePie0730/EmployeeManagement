83316f59d7d1d06d196511b62853e13e30662f88
CHEN-HSUAN MA <doll80224@gmail.com>
2025-03-23 17:53:34 +1000

    Fix mongoDB connection-2

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 
-connectDB();
+
 
 app.use(cors());
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use("/api/employees", employeeRoutes);
 
 // Export the app object for testing
 if (require.main === module) {
-    //connectDB();
+    connectDB();
     // If the file is run directly, start the server
     const PORT = process.env.PORT || 5001;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
