ddf4e63ae756eced44ddd5fe17de0b69bafbdbed
CHEN-HSUAN MA <doll80224@gmail.com>
2025-03-23 16:00:41 +1000

    Update URL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://3.27.132.93:5001', // local
-  //baseURL: 'http://13.238.218.155/', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
